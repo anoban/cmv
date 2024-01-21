1b99ca04c4015df32d8eb8aad3bec2b0b0c41350
anoban <2016sb117@univ.jfn.ac.lk>
2024-01-21 06:14:14 +0530

    new file:   .~lock.cmv.docx# 	new file:   ICTV.ipynb 	modified:   china.ipynb 	modified:   cmv.docx 	modified:   cmv.ipynb 	modified:   cmvs-corrected.odg 	modified:   data/BLAST/results.csv 	new file:   data/Venkataravanappa 2012.fasta new file:   data/degenerate_primers.fasta 	deleted:    diseasesev.png 	deleted:    findstr.ps1 	new file:   ictv.png 	new file:   locations.png deleted:    samples-corrected.png 	new file:   supplements/BLAST-Results.csv 	new file:   supplements/ICTV-Begomovirus-Complete.csv

--- findstr.ps1
+++
@@ -1,6 +0,0 @@
-$files = [System.Collections.ArrayList]::new()
-$keywords = [System.Collections.ArrayList]::new()
-
-Get-ChildItem *.fasta -Recurse | ForEach-Object {Get-Content $_ -Filter -Raw } | Select-String "Nakhon" 
-
-Get-ChildItem *.fasta -Recurse | ForEach-Object {if ((Get-Content $_ -Raw).ToString().Contains("SLCMV")) {Write-Host $_.FullName}}
